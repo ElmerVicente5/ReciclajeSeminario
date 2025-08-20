f4ef078de57d3b224b708a0c8593e414ee6bfb5e
Kevin Martinez <kemart1230@gmail.com>
2025-08-20 13:32:41 -0600

    creacion de endpoint de prueba

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import cors from 'cors';
 const app = express();
-
+import {router} from './src/routes/route.js';
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'x-api-key']
 }));
 
+app.use('/api', router);
+ 
 app.use(express.json());
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8000;
 app.listen(port,  () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
