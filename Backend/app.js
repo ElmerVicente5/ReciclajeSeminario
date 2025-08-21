6c7c56f2f31c42ac5eb2c10c466f36cafc432b79
GentoBrandon <128234033+GentoBrandon@users.noreply.github.com>
2025-08-21 10:47:13 -0600

    feat: configuracion de conexion a base de datos por URL

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ dotenv.config();
 import cors from 'cors';
 const app = express();
 import {router} from './src/routes/route.js';
+import { initializeDatabase} from './src/config/db.js';
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'OPTIONS'],
@@ -14,6 +15,16 @@ app.use('/api', router);
  
 app.use(express.json());
 const port = process.env.PORT || 8000;
-app.listen(port,  () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+
+initializeDatabase()
+  .then(() => {
+    app.listen(port,  () => {
+      console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error inicializando la base de datos:', error);
+    app.listen(port,  () => {
+      console.log(`Servidor corriendo (sin DB) en http://localhost:${port}`);
+    });
+  });
