5b883e85506faae8e36872ac8f7ae5396d004c44
Kevin Martinez <kemart1230@gmail.com>
2025-08-21 22:30:25 -0600

    Implementacion libreria JWT y configuracion de Llave secreta

---
+++ Backend/src/config/config.jwt.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+const SECRET_KEY = process.env.SECRET_KEY;
+const EXPIRES_IN = '1h';
+export const configJwt = {
+    secret: SECRET_KEY,
+    expiresIn: EXPIRES_IN
+}
