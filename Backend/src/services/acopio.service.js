d762f2eb6af05eba0f40ae8ae943425e05c9c19c
GentoBrandon <128234033+GentoBrandon@users.noreply.github.com>
2025-09-10 23:33:24 -0600

    feat: listar puntos de acopio con coordenadas

--- Backend/src/services/acopio.service.js
+++ Backend/src/services/acopio.service.js
@@ -32,3 +32,28 @@ export const obtenerAcopio = async () => {
         throw error;
     }
 };
+
+export const obtenerListadoAcopioCoordenadas = async () => {
+    try {
+        const acopio = await prisma.centrosacopio.findMany({
+            select: {
+                id: true,
+                nombre: true,
+                latitud: true,
+                longitud: true,
+            }
+        });
+
+        if (acopio.length === 0) {
+            return {
+                status: 404,
+                message: "No se encontraron acopios",
+            };
+        }
+
+        return acopio;
+    } catch (error) {
+        console.error("Error al obtener el listado de acopio coordenadas:", error);
+        throw error;
+    }
+};
