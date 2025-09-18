626222f10395bc7a0159e1af2b39d53dc78a4106
Alejandro Gallardo <agallardoc1@miumg.edu.gt>
2025-09-18 01:29:45 -0600

    feat/rankin: incluir bandera de autenticación en la respuesta del controlador y servicio

--- Backend/src/services/rankinZonas.service.js
+++ Backend/src/services/rankinZonas.service.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '../generated/prisma/client.js';
 const prisma = new PrismaClient();
-export async function rankingZonas() {
+export async function rankingZonas(userId= null) {
     // Agrupamos por zona y sumamos puntos
     try {
         const rows = await prisma.eventospuntosporzona.groupBy({
@@ -18,11 +18,22 @@ export async function rankingZonas() {
           })
           const nameById = Object.fromEntries(zonas.map(z => [z.id, z.nombre]))
           
+          // obtener la zona del usuario si el usaerId es proporcionado
+          let userZonaId = null;
+          if(userId){
+            const user = await prisma.usuarios.findUnique({
+                where: { id: userId },
+                select: { zona_id: true },
+            });
+            userZonaId = user?.zona_id || null;
+          }
+          
           // Unimos resultados
           return rows.map(r => ({
             zona_id: r.zona_id,
             zona: nameById[r.zona_id] ?? '(sin nombre)',
             total_puntos: Number(r._sum.puntos ?? 0),
+            is_user_zona: userId ? r.zona_id === userZonaId : false
           }))
     } catch (error) {
         throw error;
