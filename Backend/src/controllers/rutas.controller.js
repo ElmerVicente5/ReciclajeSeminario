808f4ac82d78e47e0aa5da376a991310bfecea50
Elmer <vicente.pelico305@gmail.com>
2025-10-08 23:56:46 -0600

    feat: cambios en documentación, logica de rutas, crud de acopios

--- Backend/src/controllers/rutas.controller.js
+++ Backend/src/controllers/rutas.controller.js
@@ -20,25 +20,25 @@ export const getRutas = async (req, res) => {
 }
 export const crearRutas = async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
     try {
-        const { nombre, zona_id } = req.body;
-        const ruta = await crearRuta({ nombre, zona_id });
+        const { nombre, zona_id, inicio_latitud, inicio_longitud, fin_latitud, fin_longitud, puntos_intermedios } = req.body;
+        const ruta = await crearRuta({ nombre, zona_id, inicio_latitud, inicio_longitud, fin_latitud, fin_longitud, puntos_intermedios });
         res.status(201).json({ message: "Ruta creada exitosamente", ruta });
     } catch (error) {
         res.status(500).json({ message: "Error al crear la ruta", error: error.message });
     }
 }
+
+
 export const actualizarRutas = async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
     try {
-        const { id, nombre, zona_id , activo } = req.body;
-        const ruta = await actualizarRuta({ id, nombre, zona_id , activo });
+        const { id, nombre, zona_id, activo, inicio_latitud, inicio_longitud, fin_latitud, fin_longitud, puntos_intermedios } = req.body;
+        const ruta = await actualizarRuta({ id, nombre, zona_id, activo, inicio_latitud, inicio_longitud, fin_latitud, fin_longitud, puntos_intermedios });
         res.status(200).json({ message: "Ruta actualizada exitosamente", ruta });
     } catch (error) {
         res.status(500).json({ message: "Error al actualizar la ruta", error: error.message });
